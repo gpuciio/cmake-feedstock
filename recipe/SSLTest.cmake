61c1a993ab8ca8c5ae5b298bf4987b8c4deb3ff2
Jordan Jacobelli <jjacobelli@nvidia.com>
2021-05-25 18:44:31 +0200

    Fix LICENSE.txt download url in test
    Signed-off-by: Jordan Jacobelli <jjacobelli@nvidia.com>

--- recipe/SSLTest.cmake
+++ recipe/SSLTest.cmake
@@ -1,5 +1,5 @@
 set(FILE_NAME "LICENSE.txt")
-set(DOWNLOAD_URL "https://raw.githubusercontent.com/conda-forge/cmake-feedstock/master/${FILE_NAME}")
+set(DOWNLOAD_URL "https://raw.githubusercontent.com/gpuciio/cmake-feedstock/v3.18.5/${FILE_NAME}")
 set(EXPECTED_SHA256 "1b17a4bbffec8473f2c0be83f697c2e533f8e1170ed74525ceeee24ddd887afd")
 
 file(DOWNLOAD ${DOWNLOAD_URL} ${CMAKE_CURRENT_BINARY_DIR}/${FILE_NAME}
